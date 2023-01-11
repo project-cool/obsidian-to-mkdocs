dd97caaa8fabb914b5ed30306289295502de8656
Sarthak Narayan <sarthak.narayang@gmail.com>
2023-01-11 18:05:09 -0500

    figuring mkdocs build error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [error]: $1"
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] [error]: $1"
 }
 
 if [ -z $1 ]; then
-    log "Please provide docs path"
-    exit 1
+	log "Please provide docs path"
+	exit 1
 fi
 docsPath=$1
 
-if [ $2 == "true" ]; then
-    mkdocs build 2> /dev/null # /app/docs => /app/site
-else 
-    mkdocs build
-fi
+# if [ $2 == "true" ]; then
+#     mkdocs build 2> /dev/null # /app/docs => /app/site
+# else
+#     mkdocs build
+# fi
+#
+# if [ $? -eq 1 ]; then
+#     log "MkDocs failed to build website"
+#     exit 1
+# fi
 
-if [ $? -eq 1 ]; then
-    log "MkDocs failed to build website"
-    exit 1
-fi
+mkdocs build
 
 rm -rf /app/docs
 
